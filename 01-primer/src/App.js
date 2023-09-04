e684d0e8a709048e86a2de3bffe89b9f9e231761
pablovirtual <pablo.gomez@udgvirtual.udg.mx>
2023-09-03 18:26:40 -0600

    valle hermoso

---
+++ 01-primer/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          FRACCIONAMIENTO VALLE HERMOSO
+        </p>
+        <ul>
+          <li>INICIO</li>
+          <li>PAGO</li>
+          <li>CONTACTO</li>
+          <li>FACTURA</li>
+          <li>COMENTARIOS</li>
+        </ul>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
